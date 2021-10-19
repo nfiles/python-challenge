4a8818a63a9dd4a5323921806a476c53054b21db
Nathan Files <nathanwfiles@gmail.com>
2021-10-19 15:44:31 -0400

    problem 5 solution

---
+++ solutions/problem005/src/main.rs
@@ -0,0 +1,23 @@
+use serde_pickle::DeOptions;
+use std::io::{self, Read};
+
+fn main() {
+    // read contents from stdin
+    let mut contents = String::new();
+    let mut stdin = io::stdin();
+    stdin.read_to_string(&mut contents).ok();
+
+    // deserialize the pickle dump
+    let deserialized: Vec<Vec<(String, i64)>> =
+        serde_pickle::from_slice(&contents.as_bytes(), DeOptions::default()).unwrap();
+
+    // print the results
+    for list in deserialized {
+        for (bytes, num) in list {
+            for _ in 0..num {
+                print!("{}", bytes);
+            }
+        }
+        println!("");
+    }
+}
